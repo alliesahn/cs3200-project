1e35bfc1d886c9eff29ad5315c50d94c2c017423
smckinless <smckinless@gmail.com>
2016-12-02 16:57:09 -0500

    app.js changes

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+
+app.get('/', function(req, res) {
+	res.sendFile('/Users/smckinless/cs3200-project/index.html');
+});
+
+app.listen(3000, function() {
+	console.log("App running on port 3000.");
+});
