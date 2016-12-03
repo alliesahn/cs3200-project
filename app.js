bf3ecbcaaa5eee2de2e7d51b5cc888171fcd930e
smckinless <smckinless@gmail.com>
2016-12-03 18:00:31 -0500

    fix directory structure and static file links

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql2');
+var path = require('path');
 
 var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'js')));
 
 app.get('/', function(req, res) {
-	connection.query('SELECT * FROM TacoRestaurant', function (err, results, fields) {
+	connection.query('SELECT * FROM Location', function (err, results, fields) {
 		console.log(results);
 	});
 	res.sendFile('/Users/smckinless/cs3200-project/index.html');
+	console.log(__dirname);
 });
 
 app.listen(3000, function() {
