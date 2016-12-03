12929cfce08679286e9d22174fe5cd14fe4d1ddf
smckinless <smckinless@gmail.com>
2016-12-03 11:37:34 -0500

    add sql stuff

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
 
 app.get('/', function(req, res) {
+	connection.query('SELECT * FROM TacoRestaurant', function (err, results, fields) {
+		console.log(results);
+	});
 	res.sendFile('/Users/smckinless/cs3200-project/index.html');
 });
 
