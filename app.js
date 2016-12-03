104cd842b9bdea845731f470aaf7504737196d44
smckinless <smckinless@gmail.com>
2016-12-03 18:21:22 -0500

    fix app.js to work on all relative directories

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ var app = express();
 var mysql = require('mysql2');
 var path = require('path');
 
-var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+//var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
 
 app.get('/', function(req, res) {
-	connection.query('SELECT * FROM Location', function (err, results, fields) {
-		console.log(results);
-	});
-	res.sendFile('/Users/smckinless/cs3200-project/index.html');
-	console.log(__dirname);
+	// connection.query('SELECT * FROM Location', function (err, results, fields) {
+	// 	console.log(results);
+	// });
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function() {
