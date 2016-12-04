61619dd59c1348bab461a4796ff60648f5be74b2
smckinless <smckinless@gmail.com>
2016-12-03 20:13:04 -0500

    neighborhood sql correct responses

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var mysql = require('mysql2');
 var path = require('path');
 
-//var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
 
@@ -12,6 +12,13 @@ app.get('/', function(req, res) {
 	// 	console.log(results);
 	// });
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/:cityName/:neighborhoodName', function(req, res) {
+	connection.query('SELECT * FROM TacoRestaurant JOIN Location ON TacoRestaurant.locationId = Location.locationId JOIN Review ON Review.storeId = TacoRestaurant.storeId JOIN Neighborhood ON Neighborhood.neighborhoodId = Location.neighborhoodId JOIN City ON City.cityId = Neighborhood.cityId WHERE Neighborhood.name = "' + req.params.neighborhoodName + '" AND City.name = "' + req.params.cityName + '";', function (err, results, fields) {
+		res.send(results);
+		console.log(results);
+	});
 });
 
 app.listen(3000, function() {
