904ab953ccba24be05fc2e9ffe43a1936da7cc1b
alliesahn <ayoung1009@gmail.com>
2016-12-03 23:25:50 -0500

    restaurant listings page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcrypt');
 var app = express();
 
 
-var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+var connection = mysql.createConnection({host:'localhost', user:'root', password:'root', database:'tacos'});
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
 //app.set('views', path.join(__dirname, app.get('assets_path') + '/views'));
@@ -31,7 +31,7 @@ app.get('/', function(req, res) {
 app.get('/:cityName/:neighborhoodName', function(req, res) {
 	connection.query('SELECT * FROM TacoRestaurant JOIN Location ON TacoRestaurant.locationId = Location.locationId JOIN Review ON Review.storeId = TacoRestaurant.storeId JOIN Neighborhood ON Neighborhood.neighborhoodId = Location.neighborhoodId JOIN City ON City.cityId = Neighborhood.cityId WHERE Neighborhood.neighborhood = "' + req.params.neighborhoodName + '" AND City.city = "' + req.params.cityName + '";', function (err, results, fields) {
 		//res.send(results);
-		results = JSON.parse(JSON.stringify(results))[0];
+		results = JSON.parse(JSON.stringify(results));
 		res.render('neighborhood.html', { results : results });
 		console.log(results);
 	});
