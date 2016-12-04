0e6ebc21c3c34b2166547b1e4022200f191ca4b5
alliesahn <ayoung1009@gmail.com>
2016-12-04 16:57:58 -0500

    google maps, soho listings, styles

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcrypt');
 var app = express();
 
 
-var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
+var connection = mysql.createConnection({host:'localhost', user:'root', password:'root', database:'tacos'});
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
 var bodyParser = require('body-parser');
@@ -28,6 +28,7 @@ app.get('/', function(req, res) {
 app.get('/:cityName/:neighborhoodName', function(req, res) {
 	connection.query('SELECT * FROM TacoRestaurant JOIN Location ON TacoRestaurant.locationId = Location.locationId JOIN Review ON Review.storeId = TacoRestaurant.storeId JOIN Neighborhood ON Neighborhood.neighborhoodId = Location.neighborhoodId JOIN City ON City.cityId = Neighborhood.cityId WHERE Neighborhood.neighborhood = "' + req.params.neighborhoodName + '" AND City.city = "' + req.params.cityName + '";', function (err, results, fields) {
 		//res.send(results);
+        console.log(results);    //
 		results = JSON.parse(JSON.stringify(results));
 		res.render('neighborhood.html', { results : results });
 		console.log(results);
