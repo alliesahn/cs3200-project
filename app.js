2e4bae915d8b529993a8e0e69ca1639f3ac806d4
smckinless <smckinless@gmail.com>
2016-12-03 22:09:14 -0500

    add nunjucks functionality

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
-var app = express();
 var mysql = require('mysql2');
 var path = require('path');
 var nunjucks = require('nunjucks');
 var bcrypt = require('bcrypt');
+var app = express();
+
 
 var connection = mysql.createConnection({host:'localhost', user:'root', password:'steviebca', database:'tacos'});
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'js')));
-app.set('views', path.join(__dirname, app.get('assets_path') + '/views'));
+//app.set('views', path.join(__dirname, app.get('assets_path') + '/views'));
+//app.set('view engine', 'njk');
 
 // Setup nunjucks templating engine
-nunjucks.configure(app.get('views'), {
+nunjucks.configure('views', {
     autoescape: true,
     noCache: true,
     watch: true,
@@ -28,10 +30,12 @@ app.get('/', function(req, res) {
 
 app.get('/:cityName/:neighborhoodName', function(req, res) {
 	connection.query('SELECT * FROM TacoRestaurant JOIN Location ON TacoRestaurant.locationId = Location.locationId JOIN Review ON Review.storeId = TacoRestaurant.storeId JOIN Neighborhood ON Neighborhood.neighborhoodId = Location.neighborhoodId JOIN City ON City.cityId = Neighborhood.cityId WHERE Neighborhood.name = "' + req.params.neighborhoodName + '" AND City.name = "' + req.params.cityName + '";', function (err, results, fields) {
-		res.send(results);
+		//res.send(results);
+		results = JSON.parse(JSON.stringify(results))[0];
+		res.render('neighborhood.html', { results : results });
 		console.log(results);
 	});
-	res.sendFile(path.join(__dirname, '/views/neighborhood.html'));
+	
 });
 
 app.listen(3000, function() {
